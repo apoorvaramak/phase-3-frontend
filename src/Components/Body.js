6ea7ee30f02dbd3718148dd59f29d964b507f6e0
Alec Magnet <amagnet@gmail.com>
2021-08-23 16:13:12 -0400

    first commit

---
+++ src/Components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BrowserRouter from 'react-router-dom'
+import { Route, Switch, useHistory } from 'react-router-dom';
+import NavBar from './NavBar';
+
+function Body () {
+
+
+}
+
+export default Body
+
+
+
