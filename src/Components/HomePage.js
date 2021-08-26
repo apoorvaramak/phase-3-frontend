18d01bc291ffd0a7d1305f2df844d988e19b819b
Apoorva <apoorvar@comcast.net>
2021-08-26 16:49:33 -0500

    added even more css

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -45,6 +45,7 @@ function HomePage({ users, setUsers, currentUser, setCurrentUser }) {
 		// setCurrentUser(updatedUser)
 	}
 
+
 	if (Object.keys(currentUser).length === 0) {
 		return(
 			<div>
@@ -62,13 +63,32 @@ function HomePage({ users, setUsers, currentUser, setCurrentUser }) {
 		)
 	} else { 
 		const books = currentUser.reviews.map((review) => {
-			console.log(review)
-			const date = review.updated_at.slice(0, 10)
+			const date = parseInt(review.updated_at.slice(0, 10))
+			const timeNow = new Date().getTime() / 1000
+			const timeDiff = parseInt((timeNow - review.updated_at))
+			const timeDisplayHours = parseInt(timeDiff/3600)
+			const timeDisplayDays = parseInt(timeDiff/(3600 * 24))
+
+			console.log(review.updated_at)
+
+			const timeToDisplay = () => {
+				if (timeDiff < 3600) {
+					return('just now')
+				} else if (timeDiff < 7200) {
+					return(`${timeDisplayHours} hour ago`)
+				} else if (timeDiff < 86400) {
+					return(`${timeDisplayHours} hours ago`)
+				} else if (timeDiff < 172800) {
+					return(`${timeDisplayDays} day ago`)
+				} else {
+					return(`${timeDisplayDays} days ago`)
+				}
+			}
 			return (
 				<div className = "the-reviews" key = {review.id}>
 					<h4><i>{review.book.title}</i> by {review.book.author}</h4>
 					<p><b>Review:</b> {review.content}</p>
-					<p style={{fontSize: 10}}>({date})</p>
+					<p style={{fontSize: 10}}>({timeToDisplay})</p>
 				</div>
 			)
 		})
